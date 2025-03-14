bdb4d038aedc17cab6aeea6809830698700064f6
Ebba <ebbe24@student.bth.se>
2025-03-14 23:25:00 +0100

    add CurrentInteractionOptions to test

--- src/gameObject.js
+++ src/gameObject.js
@@ -5,11 +5,11 @@ class GameObject{
         return listInteraction;
     };
 
-    startInteraction(theInteractionType){
-        if(theInteractionType === "look"){
+    startInteraction(InteractionType){
+        if(InteractionType === "look"){
             console.log("you looked closely")
         }
-        else if (theInteractionType === "open") {
+        else if (InteractionType === "open") {
             console.log("you open the box.")
         }
         else {
@@ -17,10 +17,135 @@ class GameObject{
         }
     };
 
-    listCurrentIneractionOptions(){};
-    setCurrentInteractionOptions(theOptions){};
 
 
+    listCurrentInteractionOptions(theOptions){
+        if(theOptions === "you are in front of Hendricks door"){
+            const listOfcurrentinteractions = ["look", "open", "inspect"];
+            return listOfcurrentinteractions;
+        }
+        else if(theOptions === "you are standing in front of a mirror" ){
+            const listOfcurrentinteractions = ["lift", "look closely"];
+            return  listOfcurrentinteractions;
+        }
+    };
 
+    setCurrentInteractionOptions(theInteractionType){
+        if(theInteractionType === "look"){
+            const setinteraction = "you looked at the carvings on the door"
+            return setinteraction
+        }
+        else if (theInteractionType === "open") {
+            const setinteraction = ("you open the door")
+            return setinteraction
+        }
+        else if (theInteractionType === "inspect") {
+            const setinteraction = ("you inspect Hendricks door closely")
+            return setinteraction
+        }
+        else if (theInteractionType === "lift"){
+            const setinteraction = ("you lifted the mirror of the huck")
+            return setinteraction
+        }
+        else if (theInteractionType === "look closely"){
+            const setinteraction = ("you look closely at the details on the mirror")
+            return setinteraction
+        }
+        else {
+            const setinteraction = ("warning! unknown interaction typ")
+            return setinteraction
+        }
+    };
+
+    // Startar en interaktion om den finns i de aktuella alternativen
+    startCurrentInteraction(starSetInteraction){
+        // if (!this.currentInteractionOptions.includes(interactionType)) {
+        //     console.error("Warning! The interaction is not available.");
+        //     return;
+        // }
+        if (starSetInteraction === "look") {
+            console.log("you looked at the carvings on the door");
+        } else if (starSetInteraction === "open") {
+            console.log("you open the door");
+        } else if ( starSetInteraction === "inspect") {
+            console.log("you inspect Hendricks door closely");
+        } else if (starSetInteraction === "lift") {
+            console.log("you lift the mirror of the huck");
+        } else if (starSetInteraction === "look closely") {
+            console.log("you look closely at the details on the mirror");
+        } else {
+            console.error("warning! unknown interaction");
+        }
+    };
+
+    // Avbryter den aktuella interaktionen (exempel på abort-funktionalitet)
+    abortCurrentInteraction(currentInteraction){
+        if (currentInteraction === null) {
+        console.error("Warning! No current interaction to abort.");
+        return;
+        }
+        else{
+            console.log(`Aborting current interaction: ${currentInteraction}`);
+            return currentInteraction = null;
+        }
+
+    };
 };
+
 module.exports = GameObject;
+
+
+
+// class GameObject {
+//     constructor() {
+//     // Initiera med en tom lista av aktuella interaktioner
+//     this.currentInteractionOptions = [];
+//     }
+
+//     // Returnerar den fullständiga listan av möjliga interaktioner
+//     listInteractionTypes = () => {
+//     return ["look", "open"];
+//     };
+
+//     // Returnerar de aktuella interaktionsalternativen
+//     listCurrentInteractionOptions = () => {
+//     return this.currentInteractionOptions;
+//     };
+
+//     // Uppdaterar de aktuella interaktionsalternativen baserat på scenario och returnerar dem
+//     setCurrentInteractionOptions = (scenario) => {
+//     if (scenario === "you are in front of Hendricks door") {
+//         this.currentInteractionOptions = ["open", "inspect"];
+//     } else if (scenario === "you are standing in front of a mirror") {
+//         this.currentInteractionOptions = ["lift", "look closely"];
+//     } else {
+//         console.error("Unknown scenario");
+//         this.currentInteractionOptions = [];
+//     }
+//     return this.currentInteractionOptions;
+//     };
+
+//     // Startar en interaktion om den finns i de aktuella alternativen
+//     startInteraction = (interactionType) => {
+//     if (!this.currentInteractionOptions.includes(interactionType)) {
+//         console.error("Warning! The interaction is not available.");
+//         return;
+//     }
+
+//     if (interactionType === "look") {
+//         console.log("you looked closely");
+//     } else if (interactionType === "open") {
+//         console.log("you open the box.");
+//     } else if (interactionType === "inspect") {
+//         console.log("you inspect Hendricks door closely");
+//     } else if (interactionType === "lift") {
+//         console.log("you lift the mirror");
+//     } else if (interactionType === "look closely") {
+//         console.log("you look closely at yourself in the mirror");
+//     } else {
+//         console.error("warning! unknown interaction");
+//     }
+//     };
+// }
+
+// module.exports = GameObject;
--- test/gameObject.test.js
+++ test/gameObject.test.js
@@ -9,6 +9,8 @@ describe("GameObject", ()=>{
         const interactions = G.listInteractionTypes();
         expect(interactions).toStrictEqual(["look", "open"])
     })
+
+
     it("Should start a interaction look", ()=>{
         const G = new GameObject ()
         G.startInteraction("look");
@@ -24,7 +26,169 @@ describe("GameObject", ()=>{
         G.startInteraction("ERROR");
         expect(consoleerror).toHaveBeenCalledWith("warning! unknown interaction")
     })
+
+
+
+
+
+    it("should return current interaction options for Hendricks door from constructor", () => {
+        const G = new GameObject();
+        const listInteractions = G.listCurrentInteractionOptions("you are in front of Hendricks door");
+        expect(listInteractions).toStrictEqual(["look", "open", "inspect"]);
+    });
+    it("should return current interaction options for mirror from constructor", () => {
+        const G = new GameObject();
+        const listInteractions = G.listCurrentInteractionOptions("you are standing in front of a mirror");
+        expect(listInteractions).toStrictEqual(["lift", "look closely"]);
+    });
+
+
+    it("should return set interaction option for look", () => {
+        const G = new GameObject();
+        const setcurrentInteractions = G.setCurrentInteractionOptions("look");
+        expect(setcurrentInteractions).toBe("you looked at the carvings on the door");
+    });
+    it("should return set interaction option for open", () => {
+        const G = new GameObject();
+        const setcurrentInteractions = G.setCurrentInteractionOptions("open");
+        expect(setcurrentInteractions).toBe("you open the door");
+    });
+    it("should return set interaction option for inspect", () => {
+        const G = new GameObject();
+        const setcurrentInteractions = G.setCurrentInteractionOptions("inspect");
+        expect(setcurrentInteractions).toBe("you inspect Hendricks door closely");
+    });
+    it("should return set interaction option for lift", () => {
+        const G = new GameObject();
+        const setcurrentInteractions = G.setCurrentInteractionOptions("lift");
+        expect(setcurrentInteractions).toBe("you lifted the mirror of the huck");
+    });
+    it("should return set interaction option for look closely", () => {
+        const G = new GameObject();
+        const setcurrentInteractions = G.setCurrentInteractionOptions("look closely");
+        expect(setcurrentInteractions).toBe("you look closely at the details on the mirror");
+    });
+    it("should return set interaction option for look closely", () => {
+        const G = new GameObject();
+        const setcurrentInteractions = G.setCurrentInteractionOptions("ERROR");
+        expect(setcurrentInteractions).toBe("warning! unknown interaction typ");
+    });
+
+
+    it("should start current set interaction options for look", () => {
+        const G = new GameObject();
+        G.startCurrentInteraction("look");
+        expect(consoleSpy).toHaveBeenCalledWith("you looked closely");
+    });
+    it("should start current set interaction options for open", () => {
+        const G = new GameObject();
+        G.startCurrentInteraction("open");
+        expect(consoleSpy).toHaveBeenCalledWith("you open the door");
+    });
+    it("should start current set interaction options for inspect", () => {
+        const G = new GameObject();
+        G.startCurrentInteraction("inspect");
+        expect(consoleSpy).toHaveBeenCalledWith("you inspect Hendricks door closely");
+    });
+    it("should start current set interaction options for lift", () => {
+        const G = new GameObject();
+        G.startCurrentInteraction("lift");
+        expect(consoleSpy).toHaveBeenCalledWith("you lift the mirror of the huck");
+    });
+    it("should start current set interaction options for look closely", () => {
+        const G = new GameObject();
+        G.startCurrentInteraction("look closely");
+        expect(consoleSpy).toHaveBeenCalledWith("you look closely at the details on the mirror");
+    });
+    it("should start current set interaction options for look closely", () => {
+        const G = new GameObject();
+        G.startCurrentInteraction("ERROR NO INTERACTION FOUND!");
+        expect(consoleerror).toHaveBeenCalledWith("warning! unknown interaction");
+    });
+
+
+    it("should log a warning if there is no current interaction", () => {
+        const G = new GameObject();
+        G.abortCurrentInteraction(null);
+        expect(consoleerror).toHaveBeenCalledWith("Warning! No current interaction to abort.");
+    });
+    it("should log aborting message and set currentInteraction to null", () => {
+        const G = new GameObject();
+        const currentInteractions = G.abortCurrentInteraction("look");
+        expect(consoleSpy).toHaveBeenCalledWith("Aborting current interaction: look");
+        currentInteractions === null;
+        // expect(gameObject.currentInteraction).toBeNull();
+    });
 })
 
 
 //toBe en sak att testa
+
+
+
+
+
+// const GameObject = require("../src/gameObject");
+
+// describe("GameObject", () => {
+// let consoleSpy;
+// let consoleErrorSpy;
+
+// beforeEach(() => {
+//     // Sätt upp spies för console.log och console.error innan varje test
+//     consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+//     consoleErrorSpy = jest.spyOn(console, "error").mockImplementation(() => {});
+// });
+
+// afterEach(() => {
+//     // Återställ spies efter varje test
+//     consoleSpy.mockRestore();
+//     consoleErrorSpy.mockRestore();
+// });
+
+// it("should return a list of interactions", () => {
+//     const gameObject = new GameObject();
+//     const interactions = gameObject.listInteractionTypes();
+//     expect(interactions).toStrictEqual(["look", "open"]);
+// });
+
+// // Eftersom startInteraction kontrollerar att interaktionen finns i currentInteractionOptions,
+// // måste vi se till att den listan innehåller de testade alternativen.
+// it("should start a 'look' interaction", () => {
+//     const gameObject = new GameObject();
+//     // Sätt currentInteractionOptions manuellt för testets skull
+//     gameObject.currentInteractionOptions = ["look", "open"];
+//     gameObject.startInteraction("look");
+//     expect(consoleSpy).toHaveBeenCalledWith("you looked closely");
+// });
+
+// it("should start an 'open' interaction", () => {
+//     const gameObject = new GameObject();
+//     gameObject.currentInteractionOptions = ["look", "open"];
+//     gameObject.startInteraction("open");
+//     expect(consoleSpy).toHaveBeenCalledWith("you open the box.");
+// });
+
+// it("should warn for an unknown interaction", () => {
+//     const gameObject = new GameObject();
+//     gameObject.currentInteractionOptions = ["look", "open"];
+//     gameObject.startInteraction("ERROR");
+//     expect(consoleErrorSpy).toHaveBeenCalledWith("warning! unknown interaction");
+// });
+
+// it("should return current interaction options for Hendricks door", () => {
+//     const gameObject = new GameObject();
+//     const currentInteractions = gameObject.setCurrentInteractionOptions(
+//     "you are in front of Hendricks door"
+//     );
+//     expect(currentInteractions).toStrictEqual(["open", "inspect"]);
+// });
+
+// it("should return current interaction options for mirror", () => {
+//     const gameObject = new GameObject();
+//     const currentInteractions = gameObject.setCurrentInteractionOptions(
+//     "you are standing in front of a mirror"
+//     );
+//     expect(currentInteractions).toStrictEqual(["lift", "look closely"]);
+// });
+// });
